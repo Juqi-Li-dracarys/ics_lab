32b0b6d37a3944d5d5288e508cd3adfc91a98f7d
tracer-nju <tracer@nju.edu.cn>
2023-12-07 17:00:49 +0800

    Static hostname: dracacys-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 06448809b4a442e295fea969dd4fff1e          Boot ID: deddcbb6d9ed437ea8c6411aa7c8e28a   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  17:00:49 up 38 min,  1 user,  load average: 0.13, 0.28, 0.34

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -12,25 +12,6 @@ static int primes[N] = {0};
 // we define prime is false
 
 int *sieve(int n) {
-  // assert(n + 1 < N);
-  // is_prime[2] = true;
-  // for (int i = 3; i <= n; i++)
-  //   is_prime[i] = (i % 2 == 0) ? false : true;
-
-  // for (int i = 3; i <= n / 2 + 1; i = i + 2) {
-  //   for (int j = i + i; j <= n; j += i) {
-  //     is_prime[j] = false;
-  //   }
-  // }
-
-  // int *p = primes;
-  // for (int i = 2; i <= n; i++)
-  //   if (is_prime[i]) {
-  //     *p++ = i;
-  //   }
-  // *p = 0;
-  // return primes;
-
   register int *p = primes;
   assert(n + 1 < N);
   for(register int i = 2; i <= n; i++) {

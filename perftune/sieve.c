4b235296656174c6745ac3292426b0c6e89c1ea9
tracer-nju <tracer@nju.edu.cn>
2023-12-08 16:27:45 +0800

    Static hostname: dracacys-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 06448809b4a442e295fea969dd4fff1e          Boot ID: 4ebb133fb1ae4d0582a3ff23d88b82de   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:27:45 up 26 min,  1 user,  load average: 0.66, 0.42, 0.33

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -13,42 +13,42 @@ static uint32_t primes[N] = {0};
 // We know the cache L1d size: 128 KiB (4 instances)
 // now implement segmented sieve
 int *sieve(int n) {
-  assert(n + 1 < N);
-  register uint64_t i = 2;
-  register uint64_t j = 0;
-  register uint32_t *p = primes;
-  register uint32_t limit = (uint32_t)n;
-
-  for(; i <= n; i++) {
-    // this number is a prime
-    if(is_prime[i] == false) {
-      *p++ = i;
-      j = i * i;
-      while (j <= limit) {
-        is_prime[j] = true;
-        j += i;
-      }
-    }
-  }
-  *p = 0;
-  return (int *)primes;
-
-
   // assert(n + 1 < N);
-  // for (int i = 0; i <= n; i++)
-  //   is_prime[i] = true;
+  // register uint64_t i = 2;
+  // register uint64_t j = 0;
+  // register uint32_t *p = primes;
+  // register uint32_t limit = (uint32_t)n;
 
-  // for (int i = 2; i <= n; i++) {
-  //   for (int j = i + i; j <= n; j += i) {
-  //     is_prime[j] = false;
+  // for(; i <= n; i++) {
+  //   // this number is a prime
+  //   if(is_prime[i] == false) {
+  //     *p++ = i;
+  //     j = i * i;
+  //     while (j <= limit) {
+  //       is_prime[j] = true;
+  //       j += i;
+  //     }
   //   }
   // }
-  // for (int i = 2; i <= n; i++)
-  //   if (is_prime[i]) {
-  //     *p++ = i;
-  //   }
   // *p = 0;
-  // return primes;
+  // return (int *)primes;
+
+
+  assert(n + 1 < N);
+  for (int i = 0; i <= n; i++)
+    is_prime[i] = true;
+
+  for (int i = 2; i <= n; i++) {
+    for (int j = i + i; j <= n; j += i) {
+      is_prime[j] = false;
+    }
+  }
+  for (int i = 2; i <= n; i++)
+    if (is_prime[i]) {
+      *p++ = i;
+    }
+  *p = 0;
+  return primes;
 
 }
 

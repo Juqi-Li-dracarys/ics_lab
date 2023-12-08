5cce5719171916e2c1ccc5269056fc3ea418da33
tracer-nju <tracer@nju.edu.cn>
2023-12-08 13:44:53 +0800

    Static hostname: dracacys-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 06448809b4a442e295fea969dd4fff1e          Boot ID: 308d940b3ba64a608ab430e3882f5432   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  13:44:53 up  1:57,  1 user,  load average: 0.68, 0.72, 0.68

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -13,10 +13,10 @@ static uint32_t primes[N] = {0};
 // We know the cache L1d size: 128 KiB (4 instances)
 // now implement segmented sieve
 uint32_t *sieve(uint32_t n) {
-  // assert(n + 1 < N);
-  // register uint32_t *p = primes;
-  // register uint32_t i = 2;
-  // register uint64_t j = 0;
+  assert(n + 1 < N);
+  register uint32_t *p = primes;
+  register uint32_t i = 2;
+  register uint64_t j = 0;
   // for(; i <= n; i++) {
   //   // this number is a prime
   //   if(is_prime[i] == false) {

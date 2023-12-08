694fdea00d97a51648f9a67a921ee482dc2a6a60
tracer-nju <tracer@nju.edu.cn>
2023-12-08 12:59:38 +0800

    Static hostname: dracacys-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 06448809b4a442e295fea969dd4fff1e          Boot ID: 308d940b3ba64a608ab430e3882f5432   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  12:59:38 up  1:12,  1 user,  load average: 0.93, 0.67, 0.61

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -3,20 +3,21 @@
 #include <assert.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdint.h>
 
 #define N 10000000
 
 static bool is_prime[N] = {0};
-static unsigned int primes[N] = {0};
+static uint32_t primes[N] = {0};
 
 // define prime is false
 // We know the cache L1d size: 128 KiB (4 instances)
 // now implement segmented sieve
-unsigned int *sieve(unsigned int n) {
+uint32_t *sieve(uint32_t n) {
   assert(n + 1 < N);
-  register unsigned int *p = primes;
-  register unsigned int i = 2;
-  register unsigned long j = 0;
+  register uint32_t *p = primes;
+  register uint32_t i = 2;
+  register uint64_t j = 0;
   for(; i <= n; i++) {
     // this number is a prime
     if(is_prime[i] == false) {

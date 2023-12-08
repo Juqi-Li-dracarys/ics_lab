9bfb5023a57629c797a243767f57353d3339a6cf
tracer-nju <tracer@nju.edu.cn>
2023-12-08 16:27:25 +0800

    Static hostname: dracacys-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 06448809b4a442e295fea969dd4fff1e          Boot ID: 4ebb133fb1ae4d0582a3ff23d88b82de   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  16:27:25 up 26 min,  1 user,  load average: 0.54, 0.38, 0.31

--- perftune/main.c
+++ perftune/main.c
@@ -5,7 +5,7 @@
 int *sieve(int n);
 
 int main() {
-  int *primes = sieve(10);
+  int *primes = sieve(10000000-2);
   int count  = 0;
   for (int *cur = primes, i = 0; *cur; cur++, i++) {
     printf("%8d", *cur);

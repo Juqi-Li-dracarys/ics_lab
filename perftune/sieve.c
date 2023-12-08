fc74dfff0a8dfff6932a38ad28ec9c0c8d5d0eb0
tracer-nju <tracer@nju.edu.cn>
2023-12-08 13:13:29 +0800

    Static hostname: dracacys-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 06448809b4a442e295fea969dd4fff1e          Boot ID: 308d940b3ba64a608ab430e3882f5432   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  13:13:29 up  1:26,  1 user,  load average: 1.22, 0.91, 0.74

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -17,15 +17,18 @@ uint32_t *sieve(uint32_t n) {
   assert(n + 1 < N);
   register uint32_t *p = primes;
   register uint32_t i = 2;
-  register uint64_t j = 0;
+  register uint32_t j = 0;
+  register uint32_t sqrt = (uint32_t)sqrt((double)n);
   for(; i <= n; i++) {
     // this number is a prime
     if(is_prime[i] == false) {
       *p++ = i;
-      j = i * (i-1);
-      while (j <= n) {
-        is_prime[j] = true;
-        j += i;
+      if(i <= sqrt) {
+        j = i * i;
+        while (j <= n) {
+          is_prime[j] = true;
+          j += i;
+        }
       }
     }
   }

b075d464495d057de02103e6a4d740d52e88ffe2
tracer-nju <tracer@nju.edu.cn>
2023-12-08 11:15:14 +0800

    Static hostname: dracacys-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 06448809b4a442e295fea969dd4fff1e          Boot ID: 308d940b3ba64a608ab430e3882f5432   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-37-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  11:15:14 up 20 min,  1 user,  load average: 0.59, 0.44, 0.30

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -2,12 +2,12 @@
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
+#include <math.h>
 
 #define N 10000000
 
 static bool is_prime[N] = {0};
 static int primes[N] = {0};
-
 
 // define prime is false
 // We know the cache L1d size: 128 KiB (4 instances)
@@ -17,11 +17,14 @@ int *sieve(int n) {
   assert(n + 1 < N);
   for(register int i = 2; i <= n; i++) {
     // this number is a prime
+    register int j = i * i;
     if(is_prime[i] == false) {
       *p++ = i;
-      for(register int j = i * i; j<= n; j += i) {
-        // this number is not a prime
-        is_prime[j] = true;
+      if(j <= n) {
+        for(; j <= n; j += i) {
+          // this number is not a prime
+          is_prime[j] = true;
+        }
       }
     }
   }

226dff84d68a3faffb1d8fe9a3dbec9ed45293d2
tracer-nju <tracer@nju.edu.cn>
2023-12-26 12:15:51 +0800

    Static hostname: dracacys-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 06448809b4a442e295fea969dd4fff1e          Boot ID: edf70b8e63344e08930015bc890ec75b   Virtualization: vmware Operating System: Ubuntu 22.04.3 LTS           Kernel: Linux 6.2.0-39-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  12:15:52 up  2:14,  1 user,  load average: 0.23, 0.38, 0.37

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -1,10 +1,30 @@
 #include "common.h"
 #include <inttypes.h>
+#include <string.h>
 
 void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
 
 static uint64_t cycle_cnt = 0;
+
+// cache 每行对应一个 block
+typedef struct cache {
+  uint8_t data[1 << BLOCK_WIDTH];
+  bool valid;
+} cache_row;
+
+// cache 总行数
+static size_t cache_row_num = 0;
+
+// cache mem ptr
+static cache_row *cache_mem = NULL;
+
+// cache 总组数
+static uint32_t cache_group_num = 0;
+
+// cache 组相连数
+static uint32_t associativity_num = 0;
+
 
 void cycle_increase(int n) { cycle_cnt += n; }
 
@@ -22,12 +42,19 @@ uint32_t cache_read(uintptr_t addr) {
 // 例如当 wmask 为 0xff 时，只写入低8比特
 // 若缺失，需要从先内存中读入数据
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
+  return;
 }
 
 // 初始化一个数据大小为 2^total_size_width B，关联度为 2^associativity_width 的 cache
 // 例如 init_cache(14, 2) 将初始化一个 16KB，4 路组相联的cache
 // 将所有 valid bit 置为无效即可
 void init_cache(int total_size_width, int associativity_width) {
+    associativity_num = 1 << associativity_width;
+    cache_group_num = 1 << (total_size_width - BLOCK_WIDTH);
+    cache_row_num = (size_t)0x1 << (total_size_width + associativity_width - BLOCK_WIDTH);
+    cache_mem = (cache_row *)realloc((void *)cache_mem, sizeof(cache_row) * cache_row_num);
+    memset(cache_mem, 0, sizeof(cache_row) * cache_row_num);
+    return;
 }
 
 
